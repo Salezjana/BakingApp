b7f81d3066bf3ef3f9162db4aa5d6eaf2c74e759
Mikołaj Rod <mikolaj.rodkiewicz@gmail.com>
2018-04-21 22:59:30 +0200

    add RecipeListFragment

--- app/src/main/java/mrodkiewicz/pl/bakingapp/ui/MainActivity.java
+++ app/src/main/java/mrodkiewicz/pl/bakingapp/ui/MainActivity.java
@@ -2,16 +2,41 @@ package mrodkiewicz.pl.bakingapp.ui;
 
 import android.os.Bundle;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import mrodkiewicz.pl.bakingapp.R;
 import mrodkiewicz.pl.bakingapp.ui.base.BaseAppCompatActivity;
+import mrodkiewicz.pl.bakingapp.ui.fragments.RecipeListFragment;
 import timber.log.Timber;
 
 public class MainActivity extends BaseAppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
 
+        setupView(savedInstanceState);
+        loadRecipes();
+    }
 
+    private void setupView(Bundle savedInstanceState) {
+        if (findViewById(R.id.fragment_container) != null) {
+            if (savedInstanceState != null) {
+                return;
+            }
+
+            RecipeListFragment firstFragment = new RecipeListFragment();
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.fragment_container, firstFragment).commit();
+        }
+
+    }
+
+    private void loadRecipes() {
+        Timber.d("loadRecipes isOnline " + isInternetEnable());
+        if (isInternetEnable()){
+
+        }
     }
 }
---
+++ app/src/main/java/mrodkiewicz/pl/bakingapp/ui/fragments/RecipeListFragment.java
@@ -0,0 +1,45 @@
+package mrodkiewicz.pl.bakingapp.ui.fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+import mrodkiewicz.pl.bakingapp.R;
+
+public class RecipeListFragment extends Fragment {
+
+    @BindView(R.id.recipe_recycleviewlist)
+    RecyclerView recipeRecycleviewlist;
+    Unbinder unbinder;
+
+    public RecipeListFragment() {
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_recipe_list, container, false);
+        unbinder = ButterKnife.bind(this, view);
+        return view;
+    }
+
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
+}

0dc208a32373fec1a489aa7c29b3a06518c3dd42
Mikołaj Rod <mikolaj.rodkiewicz@gmail.com>
2018-04-23 21:49:05 +0200

    chnage type of request from api

--- app/src/main/java/mrodkiewicz/pl/bakingapp/api/APIService.java
+++ app/src/main/java/mrodkiewicz/pl/bakingapp/api/APIService.java
@@ -1,11 +1,13 @@
 package mrodkiewicz.pl.bakingapp.api;
 
+import java.util.List;
+
 import mrodkiewicz.pl.bakingapp.models.Recipe;
 import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface APIService {
     @GET("baking.json")
-    Call<Recipe> getRecipe();
+    Call<List<Recipe>> getRecipes();
 
 }
--- app/src/main/java/mrodkiewicz/pl/bakingapp/ui/MainActivity.java
+++ app/src/main/java/mrodkiewicz/pl/bakingapp/ui/MainActivity.java
@@ -2,9 +2,9 @@ package mrodkiewicz.pl.bakingapp.ui;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
 import mrodkiewicz.pl.bakingapp.BakingApp;
 import mrodkiewicz.pl.bakingapp.R;
 import mrodkiewicz.pl.bakingapp.api.APIService;
@@ -17,11 +17,14 @@ import retrofit2.Response;
 import timber.log.Timber;
 
 public class MainActivity extends BaseAppCompatActivity {
+    private ArrayList<Recipe> recipeArrayList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+
+        recipeArrayList = new ArrayList<>();
 
         setupView(savedInstanceState);
         loadRecipes();
@@ -44,17 +47,20 @@ public class MainActivity extends BaseAppCompatActivity {
         Timber.d("loadRecipes isOnline " + isInternetEnable());
         if (isInternetEnable()){
             APIService apiService = BakingApp.getClient().create(APIService.class);
-            Call<Recipe> call;
-            call = apiService.getRecipe();
-            call.enqueue(new Callback<Recipe>() {
+            Call<List<Recipe>> call;
+            call = apiService.getRecipes();
+            Timber.d("apiService getRecipes ");
+            call.enqueue(new Callback<List<Recipe>>() {
                 @Override
-                public void onResponse(Call<Recipe> call, Response<Recipe> response) {
-                    Timber.d("Callback<Recipe> onResponse" + response.toString());
+                public void onResponse(Call<List<Recipe>> call, Response<List<Recipe>> response) {
+                    Timber.d("onResponse");
+                    recipeArrayList.addAll(response.body());
+                    Timber.d("beka z typa " + recipeArrayList.size());
                 }
 
                 @Override
-                public void onFailure(Call<Recipe> call, Throwable t) {
-
+                public void onFailure(Call<List<Recipe>> call, Throwable t) {
+                    Timber.d("onFailure");
                 }
             });
         }

75398648f7746800a856cc50a51bfab613992b10
Mikołaj R <mikolaj.rodkiewicz@gmail.com>
2018-04-28 15:36:33 +0200

    update Config

--- app/src/main/java/mrodkiewicz/pl/bakingapp/helper/Config.java
+++ app/src/main/java/mrodkiewicz/pl/bakingapp/helper/Config.java
@@ -1,5 +1,30 @@
 package mrodkiewicz.pl.bakingapp.helper;
 
+import android.content.ContentResolver;
+import android.content.ContentUris;
+import android.net.Uri;
+import android.provider.BaseColumns;
+
 public class Config {
     public static String BAKING_URL = "https://d17h27t6h515a5.cloudfront.net/topher/2017/May/59121517_baking/";
+
+    public static final String CONTENT_AUTHORITY = "mrodkiewicz.pl.bakingapp";
+
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+    public static String DATABASE_RECIPE = "recipesDB";
+    public static String TABLE_RECIPE = "recipes";
+
+    public static final String CONTENT_DIR_TYPE =
+            ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + DATABASE_RECIPE;
+
+    public static final String CONTENT_ITEM_TYPE =
+            ContentResolver.CURSOR_ITEM_BASE_TYPE +"/" + CONTENT_AUTHORITY + "/" + DATABASE_RECIPE;
+
+    public static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon()
+            .appendPath(DATABASE_RECIPE).build();
+
+    public static Uri buildFlavorsUri(long id){
+        return ContentUris.withAppendedId(CONTENT_URI, id);
+    }
 }

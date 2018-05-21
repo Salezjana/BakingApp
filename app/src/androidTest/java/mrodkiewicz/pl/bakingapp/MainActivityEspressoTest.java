403ed29c0dce6c942b09186edb544294d00a0d00
Mikołaj R <mikolaj.rodkiewicz@gmail.com>
2018-05-22 01:49:06 +0200

    write new 2 tests

--- app/src/androidTest/java/mrodkiewicz/pl/bakingapp/MainActivityEspressoTest.java
+++ app/src/androidTest/java/mrodkiewicz/pl/bakingapp/MainActivityEspressoTest.java
@@ -1,26 +1,62 @@
 package mrodkiewicz.pl.bakingapp;
 
+import android.content.pm.ActivityInfo;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.contrib.RecyclerViewActions;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.util.concurrent.TimeUnit;
+
+import mrodkiewicz.pl.bakingapp.ui.MainActivity;
+
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.hasDescendant;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.isRoot;
+import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.core.AllOf.allOf;
 
 
 @RunWith(AndroidJUnit4.class)
 public class MainActivityEspressoTest {
-
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
 
     @Test
-    public void clickListViewItem() {
+    public void landspaceRecycleView() throws InterruptedException {
+        mActivityTestRule.getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         onView(allOf(withId(R.id.recipe_recycleviewlist),
                 isDisplayed()));
 
     }
+
+    @Test
+    public void portraitRecycleView() {
+        mActivityTestRule.getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        onView(allOf(withId(R.id.recipe_recycleviewlist),
+                isDisplayed()));
+
+
+    }
+
+    @Test
+    public void clickMenuItem() {
+        onView(withContentDescription(R.string.action_refresh)).perform(click());
+    }
+
+
+
+
 
 
 }
